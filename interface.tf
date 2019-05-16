49b8fe182a1ca948cf73f2b7c2604eac6397b7ae
Neil Davies <ndavies@Neils-MacBook-Air.local>
2019-05-16 10:32:05 +0100

    Initial remote_state module

---
+++ interface.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  default     = "eu-west-2"
+  description = "The default AWS region"
+}
+
+variable "prefix" {
+  default     = "examplecom"
+  description = "The name of our Org"
+}
+
+variable "environment" {
+  default     = "development"
+  description = "The name of our environment"
+}
+
+output "s3_bucket_id" {
+  value = "${aws_s3_bucket.remote_state.id}"
+}
